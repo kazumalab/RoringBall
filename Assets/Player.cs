724e6b966dfb3133f548402f15ee000fe0a1358b
kazumalab <kazuma-arashi@hotmail.co.jp>
2017-11-03 23:43:06 +0900

    first commit

---
+++ Assets/Player.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	private Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+		rb = this.GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float dy = Input.GetAxis ("Horizontal");
+		float dx = Input.GetAxis ("Vertical");
+
+		Vector3 direction = Vector3.right * dx + Vector3.forward * dy;
+		rb.AddForce (direction);
+	}
+}
